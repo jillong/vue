c4936ce528c419b0f22b6152faf4c9b822f89c67
jillong <jillong@youyon.com>
2024-06-25 16:27:11 +0800

    增加对代码高亮的功能

---
+++ vue-one/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Markdown 编辑器</h1>
+  <textarea v-model="markdownText" placeholder="输入 Markdown 文本"></textarea>
+  <div v-highlight v-html="renderedMarkdown"></div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import {marked} from 'marked';
+
+const markdownText = ref('');
+const renderedMarkdown = computed(() => {
+  return marked(markdownText.value);
+});
+
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+}
+
+textarea {
+  width: 98%;
+  height: 200px;
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  resize: vertical;
+}
+
+div {
+  width: 98%;
+  border: 1px solid #ccc;
+  padding: 10px;
+  background-color: #f9f9f9;
+  font-family: sans-serif;
+}
+</style>
