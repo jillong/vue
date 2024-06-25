ac70be39018bafe79e67d5d969d0fe2bce733bb5
jillong <13269151756@163.com>
2024-06-25 22:53:40 +0800

    对接第一个后端请求：/chat

--- vue-one/src/App.vue
+++ vue-one/src/App.vue
@@ -1,17 +1,58 @@
 <template>
-  <h1>Markdown 编辑器</h1>
-  <textarea v-model="markdownText" placeholder="输入 Markdown 文本"></textarea>
-  <div v-highlight v-html="renderedMarkdown"></div>
+  <h1>spring ai 助手</h1>
+  <textarea v-model="userQuery" placeholder="请输入查询内容"></textarea>
+  <button @click="sendPostRequest">发送请求</button>
+
+  <div v-if="response">
+    <div v-highlight v-html="renderedMarkdown"></div>
+  </div>
+
+  <div v-if="error">
+    <h3>错误:</h3>
+    <pre>{{ error }}</pre>
+  </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue';
 import {marked} from 'marked';
+import axios from 'axios';
 
-const markdownText = ref('');
 const renderedMarkdown = computed(() => {
-  return marked(markdownText.value);
+  return marked(response.value);
 });
+
+const userQuery = ref(''); // 用户输入的查询内容
+const response = ref(null);
+const error = ref(null);
+
+const sendPostRequest = async () => {
+  const url = 'http://localhost:8090/ai/chat';
+  const data = {
+    chatOptions: {
+      stream: false,
+      augmented: false,
+    },
+    messages: [{
+      role: 'user',
+      content: '使用中文回答',
+    }],
+    query: userQuery.value,
+  };
+
+  try {
+    const res = await axios.post(url, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    response.value = res.data;
+    error.value = null;
+  } catch (error) {
+    error.value = error.message;
+    response.value = null;
+  }
+};
 
 </script>
 
@@ -25,7 +66,7 @@ const renderedMarkdown = computed(() => {
 
 textarea {
   width: 98%;
-  height: 200px;
+  height: 100px;
   margin-bottom: 20px;
   padding: 10px;
   border: 1px solid #ccc;
